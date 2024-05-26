220d0acf46459127a93b0ddb4ae59b7ffc5b1d30
Kamil Kordecki <kamil@kordecki.dev>
2024-05-26 18:04:17 +0200

    feat: Add util precisionRound

---
+++ src/utils/precisionRound.jsx
@@ -0,0 +1,6 @@
+export function precisionRound(number, precision) {
+    var factor = Math.pow(10, precision);
+    var n = precision < 0 ? number : 0.01 / factor + number;
+    return Math.round( n * factor) / factor;
+  }
+  
