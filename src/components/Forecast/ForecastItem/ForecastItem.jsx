fa3fb56b9b15dd101fcdec026f6ff54b3d7581e9
Kamil Kordecki <kamil@kordecki.dev>
2024-05-26 15:35:26 +0200

    feat: Implement weatherIcons to ForecastItem

--- src/components/Forecast/ForecastItem/ForecastItem.jsx
+++ src/components/Forecast/ForecastItem/ForecastItem.jsx
@@ -1,9 +1,10 @@
 import './ForecastItem.css';
+import weatherIcons from '../../../assets/weatherIcons';
 
 function ForecastItem({ date, weatherCode, temperatureMax, temperatureMin, energy }) {
   return (
     <div className="forecast-item">
-      <img src="weatherIcon.png" alt="Weather Icon" />
+      <img src={weatherIcons[weatherCode]} alt="Weather Icon" />
       <div>
         <div className="forecast-item-data">
           <h3>{new Date(date).toLocaleDateString("pl-PL")}</h3>
